d25feb360bb6f4da590bdc97d6d256563ac5c2a3
Yondonrinchin Batsukh <yondon898@gmail.com>
2021-10-01 22:06:40 +0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Yondonrinchin Batsukh on 2021.10.01.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
